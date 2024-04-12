338abeca2fb53007a26cbd609e6166cffd43727f
Katelyn Schiesser <katelyn.schiesser@gmail.com>
2024-04-11 17:02:41 -0700

    generate a random mac

--- src/network.cpp
+++ src/network.cpp
@@ -1,7 +1,7 @@
 #include "network.h"
 #include "config.h"
 
-byte mac[] = { 0xae, 0x03, 0xf3, 0xc7, 0x08, 0x78 };
+byte mac[6];
 uint8_t rbuf[1500];
 bool got_lldp = false;
 
@@ -11,6 +11,13 @@ void network_init(){
     digitalWrite(WIZ_RESET, HIGH);
     // give the wiznet a bit to initialize
     delay(500);
+    network_generate_mac();
     w5500.begin(mac);
 }
 
+void network_generate_mac(){
+    randomSeed(analogRead(VDIV));
+    for(uint8_t i=0;i<6;i++){
+        mac[i] = random(0, 255);
+    }
+}
--- src/network.h
+++ src/network.h
@@ -1,6 +1,7 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
+#include <Arduino.h>
 #include <w5500.h>
 
 extern byte mac[];
@@ -10,5 +11,6 @@ extern bool got_lldp;
 extern Wiznet5500 w5500;
 
 void network_init();
+void network_generate_mac();
 
 #endif
